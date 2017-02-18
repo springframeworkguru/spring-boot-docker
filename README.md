432723408724655904ab13e78ccb73da635ac9d1
John Thompson <john@springframework.guru>
2017-02-18 14:24:58 -0500

    Intial Project add

---
+++ README.md
@@ -0,0 +1,5 @@
+#Spring Boot and Docker
+
+Source code in this repo is to support my on line course for Docker and Spring Boot. 
+
+You can learn more about my course [here](http://courses.springframework.guru).
---
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -0,0 +1,35 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jt on 1/20/16.
+ */
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //@RequestMapping("/product")
+    public String getProduct(){
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@PathVariable Integer id, Model model){
+
+        model.addAttribute("product", productService.getProduct(id));
+
+        return "product";
+    }
+}
