8243b26ed0177a7095e0266b8b8f1aecc892d64b
John Thompson <john@springframework.guru>
2017-03-23 15:58:44 -0400

    added config to run group of containers

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -3,6 +3,8 @@ package guru.springframework.controllers;
 import guru.springframework.model.events.PageViewEvent;
 import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,8 @@ import java.util.UUID;
  */
 @Controller
 public class ProductController {
+
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     private ProductService productService;
     private PageViewService pageViewService;
@@ -38,6 +42,7 @@ public class ProductController {
         pageViewEvent.setPageViewDate(new Date());
         pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
 
+        log.info("Sending Message to pagie view service");
         pageViewService.sendPageViewEvent(pageViewEvent);
 
         return "product";
