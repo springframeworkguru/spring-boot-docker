51a51da907d1ffd51c9734cb45f96ff9b38ea8ac
John Thompson <john@springframework.guru>
2017-03-29 18:40:41 -0400

    completing assignment

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,10 +1,17 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by jt on 1/20/16.
@@ -12,17 +19,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private static final Logger log = LoggerFactory.getLogger(IndexController.class);
+
     private ProductService productService;
+    private PageViewService pageViewService;
 
     @Autowired
-    public IndexController(ProductService productService) {
+    public IndexController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping({"/", "index"})
     public String getIndex(Model model){
 
         model.addAttribute("products", productService.listProducts());
+
+        //Send Page view event
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/");
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        log.info("Sending Message to pagie view service");
+        pageViewService.sendPageViewEvent(pageViewEvent);
 
         return "index";
     }
