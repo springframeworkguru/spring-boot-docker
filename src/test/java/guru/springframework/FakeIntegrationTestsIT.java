3d3e04378b365d111e751b26c7841c0f9eb45ced
Marcin <maljawahi@gmail.com>
2022-11-23 14:42:03 +0100

    added fake integration test

---
+++ src/test/java/guru/springframework/FakeIntegrationTestsIT.java
@@ -0,0 +1,135 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+/**
+ * Created by jt on 3/23/17.
+ */
+public class FakeIntegrationTestsIT {
+
+    @Test
+    public void someFakeTestOne() throws Exception {
+        System.out.println("Test 1 .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 1 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 1 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 1 . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 1 . . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 1 . . . . .");
+    }
+
+    @Test
+    public void someFakeTestTwo() throws Exception {
+        System.out.println("Test 2 .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 2 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 2 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 2 . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 2 . . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 2 . . . . .");
+    }
+
+    @Test
+    public void someFakeTestThree() throws Exception {
+        System.out.println("Test 3 .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 3 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 3 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 3 . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 3 . . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 3 . . . . .");
+    }
+
+    @Test
+    public void someFakeTestFour() throws Exception {
+        System.out.println("Test 4 .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 4 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 4 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 4 . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 4 . . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 4 . . . . .");
+    }
+
+    @Test
+    public void someFakeTestFive() throws Exception {
+        System.out.println("Test 5 .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 5 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 5 . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 5 . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 5 . . . .");
+
+        Thread.sleep(100);
+
+        System.out.println("Test 5 . . . . .");
+    }
+    
+}
