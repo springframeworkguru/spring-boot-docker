a0f160bcd53cb5db0a69eded56f9ed36b71972d6
Marcin <maljawahi@gmail.com>
2022-11-23 16:34:36 +0100

    added sendPageViewEvent for index page

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,10 +1,15 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by jt on 1/20/16.
@@ -13,16 +18,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
     private ProductService productService;
+    private PageViewService pageViewService;
 
     @Autowired
-    public IndexController(ProductService productService) {
+    public IndexController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping({"/", "index"})
     public String getIndex(Model model){
 
         model.addAttribute("products", productService.listProducts());
+
+        //Send Page view event
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/index");
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        pageViewService.sendPageViewEvent(pageViewEvent);
 
         return "index";
     }
